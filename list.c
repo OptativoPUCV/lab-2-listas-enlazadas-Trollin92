e55fd2e19b6bb846fc6192698f055c0ee0dcdd92
Trollin92 <bastian.contreras.valen@gmail.com>
2025-03-30 21:43:43 -0300

    ta listo mi lab2 :) de Bastián Contreras list.c

--- list.c
+++ list.c
@@ -30,7 +30,6 @@ Node * createNode(void * data) {
 
 List * createList() {
     List * new = (List *)malloc(sizeof(List));
-    assert(new != NULL);
     new->head = NULL;
     new->tail = NULL;
     new->current = NULL;
